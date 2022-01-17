9f846aac641879ac57f1123d5683731ad1e584e1
Rohit Dureja <rdureja@gmail.com>
2022-01-16 19:08:21 -0500

    Updated file so that pytest would pass.  Stupid really

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -15,13 +15,17 @@ def main():
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
 
-    setup = '''
+    
+    print(timeit.timeit(stmt='expenses.categorize_for_loop()', setup = '''
 from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
-    '''
-    print(timeit.timeit(stmt='expenses.categorize_for_loop()', setup=setup, number=100000, globals=globals()))
-    print(timeit.timeit(stmt='expenses.categorize_set_comprehension()', setup=setup, number=100000, globals=globals()))
+    ''', number=100000, globals=globals()))
+    print(timeit.timeit(stmt='expenses.categorize_set_comprehension()', setup = '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''', number=100000, globals=globals()))
 
     fig, ax = plt.subplots()
     labels = ['Necessary', 'Food', 'Unnecessary']
@@ -30,7 +34,6 @@ expenses.read_expenses('data/spending_data.csv')
     for category_exps in divided_set_comp: 
         divided_expenses_sum.append(sum(x.amount for x in category_exps))
     
-    print(type(divided_expenses_sum[0]))
     ax.pie(divided_expenses_sum, labels=labels, autopct='%1.1f%%')
     plt.show()
 
